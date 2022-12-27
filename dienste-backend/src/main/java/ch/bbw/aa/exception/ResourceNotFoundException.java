ed922d9e86a88940866b048ba20a268d17698efc
Al-Kubaisi Abdullah, GHR-OSA-NEX-18 <Abdullah.Al-Kubaisi@swisscom.com>
2022-12-27 16:47:02 +0100

    created exception NOT FOUND whenever a required not existing db table then my app should throw exception

--- dienste-backend/src/main/java/ch/bbw/aa/exception/ResourceNotFoundException.java
+++ dienste-backend/src/main/java/ch/bbw/aa/exception/ResourceNotFoundException.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  *
  * Throw an exception when the table of DB is not existing
+ * created exception NOT FOUND whenever a required not existing db table then my app should throw exception,
+ * that exception will be send again to the client
  * @author Abdullah Al-Kubaisi
  * @version 27.12.2022
  */
