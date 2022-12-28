24674bf9a5bbeb8cafcd497315a61fe07293326e
Al-Kubaisi Abdullah, GHR-OSA-NEX-18 <Abdullah.Al-Kubaisi@swisscom.com>
2022-12-28 19:54:25 +0100

    created pojo class role

---
+++ dienste-backend/src/main/java/ch/bbw/aa/model/Role.java
@@ -0,0 +1,33 @@
+package ch.bbw.aa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * Role
+ * JPA Entity
+ * @author Abdullah Al-Kubaisi
+ * @version 27.12.2022
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "rolename", nullable = false)
+    private String rolename;
+
+
+}
