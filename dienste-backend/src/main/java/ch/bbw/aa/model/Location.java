f2cda95c0dff75f0ea8cb5363e470807e7be84fb
Al-Kubaisi Abdullah, GHR-OSA-NEX-18 <Abdullah.Al-Kubaisi@swisscom.com>
2023-01-02 12:59:43 +0100

    created pojo class locations that continue the adresses of Rosters

---
+++ dienste-backend/src/main/java/ch/bbw/aa/model/Location.java
@@ -0,0 +1,46 @@
+package ch.bbw.aa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Location
+ *
+ * @author Abdullah Al-Kubaisi
+ * @version 02.01.2023
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "locations")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "plz")
+    private int plz;
+
+    @Column(name = "place")
+    private String place;
+
+    @Column(name = "canton")
+    private String canton;
+
+    @OneToMany(mappedBy = "location")
+    private List<Roster> rosterList = new ArrayList<Roster>();
+
+}
