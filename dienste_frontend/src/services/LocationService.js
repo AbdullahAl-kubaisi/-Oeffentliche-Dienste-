6a604cab9db27ebbe44e6663b196352c54c86cf2
Al-Kubaisi Abdullah, GHR-OSA-NEX-18 <Abdullah.Al-Kubaisi@swisscom.com>
2023-01-07 17:46:46 +0100

    modified syntax

--- dienste_frontend/src/services/LocationService.js
+++ dienste_frontend/src/services/LocationService.js
@@ -3,25 +3,25 @@ import axios from 'axios';
 const LOCATION_BASE_REST_API_URL = 'http://localhost:8080/api/v1/locations';
 
 class LocationService {
-    getAllLocations() {
-        return axios.get(LOCATION_BASE_REST_API_URL);
-    }
+  getAllLocations() {
+    return axios.get(LOCATION_BASE_REST_API_URL);
+  }
 
-    createLocation(location) {
-        return axios.post(LOCATION_BASE_REST_API_URL, location);
-    }
+  createLocation(location) {
+    return axios.post(LOCATION_BASE_REST_API_URL, location);
+  }
 
-    getLocationById(locationId) {
-        return axios.get(LOCATION_BASE_REST_API_URL + '/' + locationId);
-    }
+  getLocationById(locationId) {
+    return axios.get(LOCATION_BASE_REST_API_URL + '/' + locationId);
+  }
 
-    updateLocation(locationId, location) {
-        return axios.put(LOCATION_BASE_REST_API_URL + '/' + locationId, location);
-    }
+  updateLocation(locationId, location) {
+    return axios.put(LOCATION_BASE_REST_API_URL + '/' + locationId, location);
+  }
 
-    deleteLocation(locationId) {
-        return axios.delete(LOCATION_BASE_REST_API_URL + '/' + locationId);
-    }
+  deleteLocation(locationId) {
+    return axios.delete(LOCATION_BASE_REST_API_URL + '/' + locationId);
+  }
 }
 
 export default new LocationService();
