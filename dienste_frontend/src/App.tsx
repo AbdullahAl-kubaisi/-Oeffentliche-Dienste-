326216d78d7b2946a388dad4438f0854725588e6
Al-Kubaisi Abdullah, GHR-OSA-NEX-18 <Abdullah.Al-Kubaisi@swisscom.com>
2023-01-18 19:12:24 +0100

    modified app, moved the links to conetet to have modern menu

--- dienste_frontend/src/App.tsx
+++ dienste_frontend/src/App.tsx
@@ -1,37 +1,14 @@
 import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import Menu from './components/Menu';
 import React from 'react';
-import ListPersonComponent from './components/ListPersonComponent';
-import AddRole from './components/AddRole';
-import ListRole from './components/ListRole';
-import AddPerson from './components/AddPerson';
-import AddRoster from './components/AddRoster';
-import ListRoster from './components/ListRoster';
-import ListLocation from './components/ListLocation';
-import AddLocation from './components/AddLocation';
 
 const App = (): JSX.Element => {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<ListPersonComponent />} />
-          <Route path="/persons" element={<ListPersonComponent />} />
-          <Route path="/add-person" element={<AddPerson />} />
-          <Route path="/edit-person/:id" element={<AddPerson />} />
-          <Route path="/roles" element={<ListRole />} />
-          <Route path="/add-role" element={<AddRole />} />
-          <Route path="/edit-role/:id" element={<AddRole />} />
-          <Route path="/rosters" element={<ListRoster />} />
-          <Route path="/add-roster" element={<AddRoster />} />
-          <Route path="/edit-roster/:id" element={<AddRoster />} />
-          <Route path="/locations" element={<ListLocation />} />
-          <Route path="/add-location" element={<AddLocation />} />
-          <Route path="/edit-location/:id" element={<AddLocation />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Menu />
+        </div>
+    );
 };
 
 export default App;
